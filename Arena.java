e5594efb8e12859b251dd6688aa5cec593dad381
DragonKing <83649251+Bailey-Gann@users.noreply.github.com>
2021-05-05 13:00:16 -0500

    Update Arena.java
    Adds descrriptions for chunks of code

--- Arena.java
+++ Arena.java
@@ -6,7 +6,9 @@ public class Arena
         Boss boss1 = new Boss(Functions.getInitiative(), 100, "Kronos", 11);
         Soldier soldier1 = new Soldier(Functions.getInitiative(), 100, "Bob", 10);
         boolean turn;
-
+        
+        
+        //This code checks to ensure both units are alive
         while(boss1.sendStatus() && soldier1.sendStatus())
         {
             double slam = Functions.getAttackMultiplier() * soldier1.sendAttack() * -1;
@@ -14,13 +16,16 @@ public class Arena
 
            turn = Functions.compareInitiative(boss1, soldier1);
             
+            
+            //This code beneath is to determine turn order...
            if(turn == true)
            { soldier1.changeHealth(pound);
             boss1.changeHealth(slam);
             }
            else{boss1.changeHealth(slam);
             soldier1.changeHealth(pound);}
-
+            
+            //This code displays death messages
            Functions.deathCheck(boss1, soldier1);
            Functions.endOfRound(boss1, soldier1);
 
